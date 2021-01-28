7f644d695255e43df4dff73239f064db9ac3396d
Rehan Mahmood <rehanone@gmail.com>
2021-01-28 10:06:03 -0500

    Basic code to allow account deposit.

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,13 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private int currentBalance = 0;
+
+    public int balance() {
+        return currentBalance;
+    }
+
+    public void deposit(int amount) {
+        currentBalance += amount;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,6 +1,5 @@
 package org.xpdojo.bank;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,8 +7,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
+    }
+
+    @Test
+    public void canDepositAnAmount() {
+        Account account = new Account();
+        account.deposit(100);
+        assertThat(account.balance()).isEqualTo(100);
+    }
+
+    @Test
+    public void canDepositMultipleAmounts() {
+        Account account = new Account();
+        account.deposit(100);
+        account.deposit(200);
+        assertThat(account.balance()).isEqualTo(300);
     }
 }
